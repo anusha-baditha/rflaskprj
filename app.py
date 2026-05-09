d5f931b89b1a3b3f459c6b99fe447410b848ddaa
BELLAMKONDAAMBICAKIRANMAYEE <ambica930@gmail.com>
2026-05-09 10:17:44 +0530

    second commit

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-from flask import Flask,jsonify,request
+from flask import Flask,jsonify,request,flash,redirect,url_for
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from mysql.connector import connection
-
+from werkzeug.utils import secure_filename
+from otp import genotp
+import os
 app=Flask(__name__)
-
+app.secret_key='code'
 CORS(app)
-
+BASE_DIR=os.path.abspath(os.path.dirname(__file__))
+UPLOAD_FOLDER=os.path.join(BASE_DIR,'static','uploads')
+ALLOWED_EXTENSIONS={"png",'jpg','gif','webp','jpeg'}
+MAX_CONTENT_LENGTH=6 *1024*1024 #6MB
+os.makedirs(UPLOAD_FOLDER,exist_ok=True)
 bcrypt=Bcrypt(app)
+app.config['SESSION_TYPE']='filesystem'
+app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH']=MAX_CONTENT_LENGTH
 
 mydb=connection.MySQLConnection(
     host='localhost',
@@ -85,7 +94,31 @@ def register():
             'status':'failed',
             'message':str(e)
         }),500
+def allowed_file(filename:str)->bool:
+    return "." in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
+@app.route('/api/upload',methods=['POST'])
+def upload():
 
+    item_filedata=request.files['image']
+
+    filename=item_filedata.filename
+    if item_filedata and filename:
+        if not allowed_file(filename):
+            flash('File type is not Allowed.pls give png,jpg,jpng,webp,gif')
+            return redirect(url_for('upload'))
+        orig_secure=secure_filename(filename)
+        ext=os.path.splitext(orig_secure)[1] #anusha.txt
+        filename=genotp()+ext
+        save_path=os.path.join(app.config['UPLOAD_FOLDER'],filename)
+        try:
+            item_filedata.save(save_path)
+        except Exception  as e:
+            flash('colud not store file data')
+            return redirect(url_for('upload'))
+
+    return jsonify({
+        'message':'File Uploaded'
+    })
 
 if __name__=='__main__':
 
